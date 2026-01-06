f402d4e30657ae9585dcf3cb13c576128b7c61ca
Willie Colatino <wcolatino@hotmail.com>
2026-01-06 01:45:13 -0300

    Inserção dos alerts e dos métodos com dados fictícios

--- src/app/components/carros/carrosdetails/carrosdetails.component.ts
+++ src/app/components/carros/carrosdetails/carrosdetails.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Carro } from '../../../models/carro';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carrosdetails',
@@ -13,9 +15,38 @@ import { Carro } from '../../../models/carro';
 export class CarrosdetailsComponent {
 
   carro: Carro = new Carro(0, "");
+  router = inject(ActivatedRoute); // Router para pegar parâmetro de rota
+  router2 = inject(Router); // Router de redirecionamento
 
-  salvar(){
-    alert('Salvo com sucesso!')
+  constructor() { // NO construtor - acessar a variável de rota
+    let id = this.router.snapshot.params['id'];
+    if (id > 0) {
+      this.findById(id);
+    }
+  }
+
+  save() {
+    if (this.carro.id > 0) { //Se o Id foi maior que zero, é atualização
+      Swal.fire({
+        text: "Editado com sucesso!",
+        icon: "success",
+        confirmButtonText: 'Ok'
+      });
+      this.router2.navigate(['admin/carros'], { state: { carroEditado: this.carro } }); //Após salvar com sucesso, redireciona para a rota de carros e envia o carroNovo salvo
+    } else {
+      Swal.fire({
+        text: "Salvo com sucesso!",
+        icon: "success",
+        confirmButtonText: 'Ok'
+      });
+      this.router2.navigate(['admin/carros'], { state: { carroNovo: this.carro } }); //Após salvar com sucesso, redireciona para a rota de carros e envia o carroNovo salvo
+    }
+  }
+
+  findById(id: number) {
+    //Busca no back-end
+    let carroRetornado: Carro = new Carro(id, "Fiesta");
+    this.carro = carroRetornado;
   }
 
 }
--- src/app/components/carros/carroslist/carroslist.component.ts
+++ src/app/components/carros/carroslist/carroslist.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Carro } from '../../../models/carro';
 import { RouterLink } from "@angular/router";
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-carroslist',
   standalone: true,
@@ -13,14 +15,57 @@ export class CarroslistComponent {
 
   lista: Carro[] = []
 
-  constructor(){
+  constructor() {
+
+    //Pega o carro editado ou cadastrado
+
+
+    //Cria a lista
     this.lista.push(new Carro(1, 'Fiesta'));
     this.lista.push(new Carro(2, 'Monza'));
     this.lista.push(new Carro(3, 'Ka'));
+
+    //Verifica no estado se é novo ou editado
+    let carroNovo = history.state.carroNovo;
+    let carroEditado = history.state.carroEditado;
+
+    //Se for novo, acidiona na lista com novo Id
+    if (carroNovo) {
+      carroNovo.id = 555
+      this.lista.push(carroNovo) //Insere novo na lista e pronto
+    }
+
+    //Se for editado, pega o indice do carro editado e devolte com os dados editatos
+    if (carroEditado) {
+      let indice = this.lista.findIndex(x => { return x.id == carroEditado.id }); //pega o indice do caro editado - Altera o objeto mas permanece no index da lista
+      this.lista[indice] = carroEditado; //Devolve o carro editado
+    }
+
+
+
   }
 
-  deletar(){
+  deleteById(carro: Carro) {
 
+    Swal.fire({
+      title: "Tem certeza que deseja deletar o registro?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        let indice = this.lista.findIndex(x => { return x.id == carro.id }); //pega o indice do caro editado
+        this.lista.splice(indice, 1);
+
+        Swal.fire({
+          title: "Registro apagado!",
+          icon: "success"
+        });
+      }
+    });
   }
 
 }
