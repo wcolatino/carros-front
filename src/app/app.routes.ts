679e1fb1d927a4c8058b00bf9dd749751c329765
Willie Colatino <wcolatino@hotmail.com>
2026-01-05 22:54:49 -0300

    Inseridas as novas rotas e o componente de menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,20 @@ import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { CarroslistComponent } from './components/carros/carroslist/carroslist.component';
 import { CarrosdetailsComponent } from './components/carros/carrosdetails/carrosdetails.component';
+import { MarcaslistComponent } from './components/marcas/marcaslist/marcaslist.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: 'full'},
     {path: "login", component: LoginComponent},
     {path: "admin", component: PrincipalComponent, children:[ //Carrega rotas filhas dentro de admin, onde o componente principal sempre renderizará os menus e s os demais componentes
+
         {path: "carros", component: CarroslistComponent},
         {path: "carros/new", component: CarrosdetailsComponent},
-        {path: "carros/edit", component: CarrosdetailsComponent},
+        {path: "carros/edit/:id", component: CarrosdetailsComponent},
+
+        {path: "marcas", component: MarcaslistComponent},
+        {path: "marcas/new", component: MarcaslistComponent},
+        {path: "marcas/edit/:id", component: MarcaslistComponent},
     ]},
 
 ];
