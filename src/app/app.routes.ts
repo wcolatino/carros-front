1c81d3de2d84d29642122672fb85aa3bce0450fe
Willie Colatino <wcolatino@hotmail.com>
2026-01-05 21:14:55 -0300

    Criação do projeto - componentes de carros, layout (principal, menu e login), carros e marcas, model de carro e marcas, criação e configuração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/layout/login/login.component';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { CarroslistComponent } from './components/carros/carroslist/carroslist.component';
+import { CarrosdetailsComponent } from './components/carros/carrosdetails/carrosdetails.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "login", pathMatch: 'full'},
+    {path: "login", component: LoginComponent},
+    {path: "admin", component: PrincipalComponent, children:[ //Carrega rotas filhas dentro de admin, onde o componente principal sempre renderizará os menus e s os demais componentes
+        {path: "carros", component: CarroslistComponent},
+        {path: "carros/new", component: CarrosdetailsComponent},
+        {path: "carros/edit", component: CarrosdetailsComponent},
+    ]},
+
+];
